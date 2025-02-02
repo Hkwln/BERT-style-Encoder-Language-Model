d82de5f54dd6f72e4a6961296a4a0bd2cc8eaa31
hkwln <p.aul@mail.de>
2025-02-02 14:29:25 +0100

    load and prepare the Stanford NLP SST dataset

--- ds_prep.py
+++ ds_prep.py
@@ -1,1 +1,5 @@
 #Here the plan is to load and prepare the dataset loading the stanfordnlp/sst2 dataset
+import torch
+from datasets import load_dataset
+dataset = load_dataset("Stanfordnlp/sst")
+print(dataset)
